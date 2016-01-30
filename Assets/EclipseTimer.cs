5db8bd9ead514ac8900ad6d111ceed1f89fcec3c
craigiedon <craiginnes@gmail.com>
2016-01-30 11:46:18 +0000

    Added eclipse timer and ritual manager tag

---
+++ Assets/EclipseTimer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EclipseTimer : MonoBehaviour {
+
+    private bool finished = false;
+    public float eclipseLength = 10f;
+    private float currentTime = 0f;
+    private RitualManager ritualManager;
+
+    void Start()
+    {
+        ritualManager = GameObject.FindGameObjectWithTag("RitualManager").GetComponent<RitualManager>();
+    }
+	
+	void Update ()
+	{
+	    currentTime += Time.deltaTime;
+	    if (!finished && currentTime > eclipseLength)
+	    {
+	        finished = true;
+	        ritualManager.Finish();
+            GetComponent<SpriteRenderer>().color = Color.red;
+	    }
+	}
+}
