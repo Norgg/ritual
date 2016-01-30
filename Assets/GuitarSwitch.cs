aa3c69ec9055fc46874df679dda61330f7fa8bd5
craigiedon <craiginnes@gmail.com>
2016-01-30 17:14:28 +0000

    Added in switches and monitors

---
+++ Assets/GuitarSwitch.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuitarSwitch : MonoBehaviour
+{
+    public bool switchOn = false;
+
+    void OnMouseDown()
+    {
+        if (switchOn)
+        {
+            switchOn = false;
+            transform.eulerAngles = new Vector3(0,0,0);
+        }
+        else
+        {
+            switchOn = true;
+            transform.eulerAngles = new Vector3(0,0,180);
+        }
+
+        GetComponent<AudioSource>().Play();
+    }
+}
