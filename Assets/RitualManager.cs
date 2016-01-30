7eefaefe47fc076d49d0a4d7a6e77d1af8ca9725
craigiedon <craiginnes@gmail.com>
2016-01-30 11:06:41 +0000

    Added ritual manager, and some organisational folders

---
+++ Assets/RitualManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RitualManager : MonoBehaviour
+{
+
+    private float successProbability = 0;
+
+	void Start ()
+	{
+	    successProbability = 1;
+	}
+
+    void ContributeProbability(float probability)
+    {
+        successProbability += Mathf.Clamp(successProbability + probability, 0, 1);
+    }
+
+
+    public void Finish()
+    {
+        Judge();
+    }
+
+    void Judge()
+    {
+        print("You suck");
+    }
+}
