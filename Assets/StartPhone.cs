b0162e53e8787477175cfa86eed5674b1d09e28b
John Turner <norgg@norgg.org>
2016-01-31 14:49:39 +0000

    Answer sound for phone in start scene.

--- Assets/StartPhone.cs
+++ Assets/StartPhone.cs
@@ -5,17 +5,27 @@ public class StartPhone : MonoBehaviour {
 	bool ringing = false;
 	public GameObject phoneTop;
 	AudioSource ringSound;
+	AudioSource answerSound;
 	private Vector3 phoneTopOrigin;
 
 	// Use this for initialization
 	void Start () {
-		ringSound = GetComponent<AudioSource>();
+		ringSound = GetComponents<AudioSource>()[0];
+		answerSound = GetComponents<AudioSource>()[1];
 		phoneTopOrigin = phoneTop.transform.position;
+	}
+
+	public void OnMouseDown() {
+		if (ringing) {
+			ringing = false;
+			ringSound.Stop();
+			answerSound.Play();
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Random.value < 0.0001) {
+		if (Random.value < 0.0001 && !answerSound.isPlaying) {
 			ringing = true;
 		}
 
