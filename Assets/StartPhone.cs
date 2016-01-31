27a8d6d4725fec2dcbd9608e15c5e03f8bc47cad
John Turner <norgg@norgg.org>
2016-01-31 14:38:16 +0000

    Make phone on start screen start ringing sometimes.

---
+++ Assets/StartPhone.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartPhone : MonoBehaviour {
+	bool ringing = false;
+	public GameObject phoneTop;
+	AudioSource ringSound;
+	private Vector3 phoneTopOrigin;
+
+	// Use this for initialization
+	void Start () {
+		ringSound = GetComponent<AudioSource>();
+		phoneTopOrigin = phoneTop.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Random.value < 0.0003) {
+			ringing = true;
+		}
+
+		if (ringing) {
+			Vector3 phonePeturb = Random.insideUnitCircle * 0.05f;
+			phoneTop.transform.position = phoneTopOrigin + phonePeturb;
+
+			if (!ringSound.isPlaying)
+			{
+				ringSound.Play();
+			}
+		}
+	
+	}
+}
