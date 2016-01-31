c400d445cfcd4f8c3c3b58579da1d76d98ee3e7d
craigiedon <craiginnes@gmail.com>
2016-01-31 12:09:57 +0000

    Moon now actually gets eclipsed

--- Assets/EclipseTimer.cs
+++ Assets/EclipseTimer.cs
@@ -7,17 +7,24 @@ public class EclipseTimer : MonoBehaviour {
     public float eclipseLength = 10f;
     private float currentTime = 0f;
     private RitualManager ritualManager;
-	new private Light light;
+	public Light directionalLight;
+    public Light pointMoonLight;
+    public GameObject lunarCover;
 
     void Start()
     {
         ritualManager = GameObject.FindGameObjectWithTag("RitualManager").GetComponent<RitualManager>();
-		light = GetComponent<Light>();
     }
 	
 	void Update ()
 	{
-		light.intensity = (1.0f - currentTime / eclipseLength) * 0.4f + (currentTime / eclipseLength) * 0.1f;
+	    directionalLight.intensity = Mathf.Lerp(0.35f, 0.1f, currentTime/eclipseLength);
+	    if (currentTime/eclipseLength < 1)
+	    {
+	        pointMoonLight.intensity = Mathf.Lerp(2f, 0.2f, currentTime/eclipseLength);
+	    }
+        lunarCover.transform.localPosition = new Vector3(Mathf.Lerp(-0.9f, 0, currentTime/eclipseLength),
+	        lunarCover.transform.localPosition.y, lunarCover.transform.localPosition.z);
 
 	    if (currentTime > eclipseLength)
 	    {
